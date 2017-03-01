0ac85d0a33cb0f28efc37ae7d45f1291da39c5f9
Diogo Cardoso <Diogo Cardoso>
2017-03-01 16:40:58 +0000

    clean-up code

--- server/routes/tvshows.js
+++ server/routes/tvshows.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  TvShows.getTvShows(req.user.id, req.cookies.userSettings)
+  TvShows.getTvShows(req.cookies.userSettings)
     .then((tv) => {
       res.status(200).send(tv);
     });
--- server/routes/weather.js
+++ server/routes/weather.js
@@ -3,41 +3,51 @@ const express = require('express');
 
 const router = express.Router();
 
-// Conditions
 router.get('/conditions', (req, res) => {
   const locationCountry = req.cookies.userSettings.weather.location.country;
   const locationCity = req.cookies.userSettings.weather.location.city;
 
-  Weather.getConditions(req.user.id, locationCountry, locationCity)
-    .then((conditions) => {
-      const baseObj = conditions.current_observation;
-      res.status(200).send({
-        location: baseObj.display_location.full,
-        temperature: baseObj.temp_c,
-        humidity: baseObj.relative_humidity,
-        description: baseObj.weather,
-        icon: baseObj.icon,
-        localtime: baseObj.local_time_rfc822,
-        lastupdate: baseObj.observation_time_rfc822,
+  if(!locationCountry || locationCountry.length === 0 || locationCountry === 'null') {
+    res.status(200).send({error: 'You need to set a County to get the Weather.'});
+  } else if (!locationCity || locationCity.length === 0 || locationCity === 'null') {
+    res.status(200).send({error: 'You need to set a City to get the Weather.'});
+  } else {
+    Weather.getConditions(locationCountry, locationCity)
+      .then((conditions) => {
+        const baseObj = conditions.current_observation;
+        res.status(200).send({
+          location: baseObj.display_location.full,
+          temperature: baseObj.temp_c,
+          humidity: baseObj.relative_humidity,
+          description: baseObj.weather,
+          icon: baseObj.icon,
+          localtime: baseObj.local_time_rfc822,
+          lastupdate: baseObj.observation_time_rfc822,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  }
 });
 
-// Forecast
 router.get('/forecast', (req, res) => {
   const locationCountry = req.cookies.userSettings.weather.location.country;
   const locationCity = req.cookies.userSettings.weather.location.city;
 
-  Weather.getForecast(req.user.id, locationCountry, locationCity)
-    .then((forecast) => {
-      res.status(200).send(forecast);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  if(!locationCountry || locationCountry.length === 0 || locationCountry === 'null') {
+      res.status(200).send({error: 'You need to set a County to get the Weather.'});
+    } else if (!locationCity || locationCity.length === 0 || locationCity === 'null') {
+      res.status(200).send({error: 'You need to set a City to get the Weather.'});
+    } else {
+      Weather.getForecast(locationCountry, locationCity)
+        .then((forecast) => {
+          res.status(200).send(forecast);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
 });
 
 module.exports = router;
