9d5468215da699fef37c0c7101a1b12fe324b072
Diogo Cardoso <diogocardoso92@outlook.com>
2017-03-11 20:49:40 +0000

    improved login/signup front-end ux

--- server/models/weather.js
+++ server/models/weather.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const rp = require('request-promise');
 const Promise = require('bluebird');
+const moment = require('moment');
 
 const Weather = {
   apiKey: process.env.WU_API_KEY,
@@ -14,15 +15,16 @@ const Weather = {
       rp(options)
         .then((res) => {
           const baseObj = res.current_observation;
-          return resolve({
+          const conditions = {
             location: baseObj.display_location.full,
             temperature: baseObj.temp_c,
             humidity: baseObj.relative_humidity,
             description: baseObj.weather,
-            icon: baseObj.icon,
+            icon: (moment().format('hh') < 7 || moment().format('hh') > 20) ? baseObj.icon : `nt_${baseObj.icon}`,
             localtime: baseObj.local_time_rfc822,
             lastupdate: baseObj.observation_time_rfc822,
-          });
+          };
+          return resolve(conditions);
         })
         .catch(err => reject(err));
     });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,29 +10,29 @@ const router = express.Router();
 router.post('/login', (req, res, next) => {
   const email = req.body.email;
   if (!validator.isEmail(email) || validator.isEmpty(email)) {
-    return res.status(500).json({ status: 'Invalid Email' });
+    return res.status(400).json({ error: 'Invalid Email' });
   }
   const password = req.body.password;
   if (validator.isEmpty(password)) {
-    return res.status(500).json({ status: 'Invalid Password' });
+    return res.status(400).json({ error: 'Invalid Password' });
   }
 
   req.body.email = validator.normalizeEmail(email);
 
   passport.authenticate('local', (err, user) => {
-    if (err) { return res.status(500).json({ status: 'Error in passport authenticate' }); }
-    if (!user) { return res.status(404).json({ status: 'User not found' }); }
+    if (err) { return res.status(500).json({ error: 'Error in passport authenticate' }); }
+    if (!user) { return res.status(404).json({ error: 'User not found' }); }
     if (user) {
       console.log(chalk.blue('User %s connected'), user.id);
       User.getUserSettingsFromFile(user.id)
         .then(userSettings => res.cookie('userSettings', userSettings))
         .then(() => {
           req.logIn(user, (err) => {
-            if (err) { return res.status(500).json({ status: 'Error in passport logIn' }); }
+            if (err) { return res.status(500).json({ error: 'Error in passport logIn' }); }
             return res.status(200).json({ status: 'success' });
           });
         })
-        .catch(err => res.status(500).json({ status: err }));
+        .catch(err => res.status(500).json({ error: err }));
     }
   })(req, res, next);
 });
@@ -47,6 +47,6 @@ router.post('/signup', (req, res, next) =>
     }))
     .then(user => User.createUserSettings(user, req.acceptsLanguages()[0]))
     .then(() => res.status(200).json({ status: 'success' }))
-    .catch(err => res.status(500).json({ status: err })));
+    .catch(err => res.status(500).json({ error: err })));
 
 module.exports = router;
