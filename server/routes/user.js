77100c4fb7a97a218cc0d898f8a6e432751f34e7
Diogo Cardoso <tuniq@live.com.pt>
2017-04-11 15:08:17 +0100

    added user routes to edit settings + front-end ajax calls

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,17 +4,42 @@ import User from '../models/user';
 
 const router = express.Router();
 
-/*
-Desc: Update user settings (add/remove/edit)
-Params: userID, setting Name, setting Data
-  settingName needs to be a key of the userSettings json template.
-Return: Updates userSettings json file and userSettings cookies
-*/
+// Edit setting (single-valued keys or multi-value keys)
 router.put('/settings', (req, res) => {
   const userID = req.user;
   const settingName = req.body.settingName;
   const settingData = req.body.settingData;
-  User.updateUserSettings(userID, settingName, settingData)
+  User.editUserSettings(userID, settingName, settingData)
+    .then((userSettings) => {
+      res.cookie('userSettings', JSON.stringify(userSettings)).status(200).send();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Server error');
+    });
+});
+
+// Add value to setting (multi-value keys)
+router.patch('/settings', (req, res) => {
+  const userID = req.user;
+  const settingName = req.body.settingName;
+  const settingData = req.body.settingData;
+  User.addUserSettings(userID, settingName, settingData)
+    .then((userSettings) => {
+      res.cookie('userSettings', JSON.stringify(userSettings)).status(200).send();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Server error');
+    });
+});
+
+// Remove value from setting (multi-value keys)
+router.delete('/settings', (req, res) => {
+  const userID = req.user;
+  const settingName = req.body.settingName;
+  const settingData = req.body.settingData;
+  User.removeUserSettings(userID, settingName, settingData)
     .then((userSettings) => {
       res.cookie('userSettings', JSON.stringify(userSettings)).status(200).send();
     })
