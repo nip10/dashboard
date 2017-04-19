79cef70c0180bfcfd36a3c264151465d9ea709da
Diogo Cardoso <tuniq@live.com.pt>
2017-04-19 19:53:00 +0100

    cleaned up code
    I was returning promisses that were returning another promise (nested) which I didnt have to

--- server/models/movies.js
+++ server/models/movies.js
@@ -1,5 +1,4 @@
 import rp from 'request-promise';
-import Promise from 'bluebird';
 
 const Movies = {
 
@@ -9,15 +8,13 @@ const Movies = {
       uri: 'https://yts.ag/api/v2/list_movies.json?sort=date_added&limit=2',
       json: true,
     };
-    return new Promise((resolve, reject) => {
-      rp(options)
-        .then((res) => {
-          imgArray.push(res.data.movies[0].medium_cover_image);
-          imgArray.push(res.data.movies[1].medium_cover_image);
-          resolve(imgArray);
-        })
-        .catch(err => reject(err));
-    });
+    return rp(options)
+      .then((res) => {
+        imgArray.push(res.data.movies[0].medium_cover_image);
+        imgArray.push(res.data.movies[1].medium_cover_image);
+        return imgArray;
+      })
+      .catch(err => console.log(err));
   },
 };
 
