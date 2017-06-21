920d9b11c5abbe5b42f34077cc54ec97bbb1735b
Diogo Cardoso <tuniq@live.com.pt>
2017-06-21 17:34:05 +0100

    Revert "updated migrations files"
    This reverts commit 552e69d9b63040eb62dfec5684713018983af592.

--- server/db/migrations/04-tvshows.js
+++
@@ -1,8 +0,0 @@
-exports.up = (knex, Promise) => knex.schema.createTableIfNotExists('tvshows', (table) => {
-  table.increments('id');
-  table.string('name').unique().notNullable();
-  table.timestamp('createdAt').notNullable().defaultTo(knex.raw('now()'));
-  table.timestamp('updatedAt').defaultTo(knex.raw('now()'));
-});
-
-exports.down = (knex, Promise) => knex.schema.dropTable('tvshows');
