f4f3b0a129f8e871173ea55f3b645a72e52931e2
Diogo Cardoso <diogocardoso92@outlook.com>
2017-06-21 17:34:05 +0100

    Revert "updated migrations files"
    This reverts commit 552e69d9b63040eb62dfec5684713018983af592.

--- server/db/migrations/03-languages.js
+++
@@ -1,7 +0,0 @@
-exports.up = (knex, Promise) => knex.schema.createTableIfNotExists('languages', (table) => {
-  table.increments('id');
-  table.string('code').unique().notNullable();
-  table.timestamp('createdAt').notNullable().defaultTo(knex.raw('now()'));
-});
-
-exports.down = (knex, Promise) => knex.schema.dropTable('languages');
