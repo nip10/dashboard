673a38512f996185c01c8abe535eb0911525cd95
Diogo Cardoso <diogocardoso92@outlook.com>
2017-08-29 00:22:13 +0100

    Updated controllers
    Updated controllers to use the DB instead of the old json logic. Some refactoring and improvements.

--- server/controllers/tvshows.js
+++ server/controllers/tvshows.js
@@ -11,17 +11,18 @@ const endInterval = '2017-02-06';
 const TvShows = {
   getTvShows(tvshows) {
     return knex('tvshows')
-        .whereIn('show', tvshows)
-        .whereBetween('airdate', [startInterval, endInterval])
-        .orderBy('airdate', 'asc')
+        .whereIn('name', tvshows)
+        .join('episodes', 'episodes.tvshow_id', '=', 'tvshows.id')
+        .whereBetween('episodes.airdate', [startInterval, endInterval])
+        .orderBy('episodes.airdate', 'asc')
         .then((result) => {
           result.forEach((element) => {
             element.airdate = moment(element.airdate).format('DD-MM-YYYY');
           });
-          const tvShowsArray = result.reduce((acc, { show, season, episode, airdate }) => {
+          const tvShowsArray = result.reduce((acc, { name, season, num, airdate }) => {
             acc.i += acc.airdate !== airdate;
             acc.airdate = airdate;
-            acc.obj[`day${acc.i}`] = (acc.obj[`day${acc.i}`] || []).concat(`${show} S${(`0${season}`).slice(-2)}E${(`0${episode}`).slice(-2)}`);
+            acc.obj[`day${acc.i}`] = (acc.obj[`day${acc.i}`] || []).concat(`${name} S${(`0${season}`).slice(-2)}E${(`0${num}`).slice(-2)}`);
             return acc;
           }, { i: 0, obj: {} }).obj;
           return tvShowsArray;
