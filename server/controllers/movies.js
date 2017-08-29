63092178778cc66c5ada681fd0106bb15e39ef07
Diogo Cardoso <tuniq@live.com.pt>
2017-08-29 02:11:41 +0100

    Minor tweaks

--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -1,20 +1,19 @@
 import rp from 'request-promise';
 
 const Movies = {
-
   getMovies() {
-    const imgArray = [];
+    const movieImages = [];
     const options = {
       uri: 'https://yts.ag/api/v2/list_movies.json?sort=date_added&limit=2',
       json: true,
     };
     return rp(options)
       .then((res) => {
-        imgArray.push(res.data.movies[0].medium_cover_image);
-        imgArray.push(res.data.movies[1].medium_cover_image);
-        return imgArray;
+        movieImages.push(res.data.movies[0].medium_cover_image);
+        movieImages.push(res.data.movies[1].medium_cover_image);
+        return movieImages;
       })
-      .catch(err => console.log(err));
+      .catch(err => err);
   },
 };
 
